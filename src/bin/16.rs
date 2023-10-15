696046b10a1f3e5fa8f8ae0f4349676015010215
Arthur Caillaud <arthur.caillaud@outmind.fr>
2023-10-15 17:56:30 +0200

    chore(day-16): download part 1

---
+++ src/bin/16.rs
@@ -0,0 +1,24 @@
+use advent_of_code::helpers::*;
+use advent_of_code::solve;
+
+fn main() {
+    let input = &read_input(16);
+    solve!(1, solve_part_1, input);
+    solve!(2, solve_part_2, input);
+}
+
+fn solve_part_1(input: &str) -> Option<u64> {
+    None
+}
+
+fn solve_part_2(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {}
+}
